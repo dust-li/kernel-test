577cfb6adc5a1588a6c1b4ea6d6614304d541084
Dust Li <dust.li@linux.alibaba.com>
2020-09-08 10:32:13 +0800

    exceed_udp_mem_min_drop: add multi-sockets test
    The problem is not for just 1 socket, with the number
    of UDP sockets grows. The total amount of udp prot memory
    will finally exceed udp_mem[0] and packets will be dropped.
    
    Signed-off-by: Dust Li <dust.li@linux.alibaba.com>

---
+++ exceed_udp_mem_min_drop/multi_sockets_test.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+if [ `whoami` != root ];then
+	echo "run as root"
+	exit 1
+fi
+
+saved_udp_mem=$(cat /proc/sys/net/ipv4/udp_mem)
+saved_rmem_default=$(cat /proc/sys/net/core/rmem_default)
+saved_rmem_max=$(cat /proc/sys/net/core/rmem_max)
+
+new_udp_mem='91944   122592  183888'
+new_rmem_default=$((2*4096*4096))
+new_rmem_max=$((2*4096*4096))
+
+# 1. setting udp_mem and rmem_xxx
+echo $new_udp_mem > /proc/sys/net/ipv4/udp_mem
+echo $new_rmem_default > /proc/sys/net/core/rmem_default
+echo $new_rmem_max > /proc/sys/net/core/rmem_max
+
+# Start the Udp server
+python <<EOF &
+#!/usr/bin/env python2
+
+import socket
+import time
+
+ServerPort = 13344
+ServerSocketCnt = 16
+
+server_sock = []
+for i in range(ServerSocketCnt):
+        address = ('0.0.0.0', ServerPort + i)
+        server_sock.append(socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
+        server_sock[i].bind(address)
+        print "server rcvbuf: %d" % server_sock[i].getsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF)
+
+time.sleep(1000)
+EOF
+server_pid=$!
+usleep 300000
+
+
+# 2. start perf to watch sock:sock_exceed_buf_limit
+perf trace -e 'sock:sock_exceed_buf_limit' &
+perf_pid=$!
+echo "Wait 2s for perf to start"
+sleep 2
+
+
+# 3. start client
+python <<EOF
+#!/usr/bin/env python2
+
+import socket
+
+ServerPort = 13344
+ServerSocketCnt = 16
+
+for i in range(ServerSocketCnt):
+        address = ('127.0.0.1', ServerPort + i)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        msg4096 = "".join(['0' for i in range(4096)])
+
+        # 3000 makes sure udp_prot.memory_allocated will exceed udp_mem[0]
+        # and sk_rmem_alloc won't exceed sk_rcvbuf
+        for _ in range(3000):
+            sock.sendto(msg4096, address)
+EOF
+
+
+# 4. Cleanup & Recover
+kill $server_pid
+kill $perf_pid
+
+echo $saved_udp_mem > /proc/sys/net/ipv4/udp_mem
+echo $saved_rmem_default > /proc/sys/net/core/rmem_default
+echo $saved_rmem_max > /proc/sys/net/core/rmem_max
